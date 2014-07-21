ffa9895fe6d34a75d452f05699376e6e20024d9c
Shehryar Mansoor <shehryar.ahmad96@gmail.com>
2014-07-21 17:47:35 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,64 @@
-console.log('This would be the main JS file.');
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <link href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/shift.css" rel="stylesheet">
+    
+    <link rel="stylesheet" href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/bootstrap.css">
+    <link rel="stylesheet" href="main.css">
+    
+  </head>
+
+  <body>
+    <div class="nav">
+      <div class="container">
+     
+        <ul class="pull-right">
+        <ul>
+          <li><a href="#">About Me</a></li>
+          <li><a href="https://github.com/shehryarmansoor">GitHub </a></li>
+          <li><a href="https://uwaterloo.ca">University</a></li>
+          <li><a href="https://cs.uwaterloo.ca/"> Computer Science </a></li>
+        </ul>
+        </ul>
+      </div>
+    </div>
+
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Shehryar Mansoor.</h1>
+        <p>A new and dedicated Computer Science student!</p>
+        <a href="https://www.facebook.com/Sherry.96">Get to know me!</a>
+      </div>
+    </div> 
+    
+    <div class="neighborhood-guide">
+    <div class="container">
+    <h2> Who am I? <h2>
+    <p> I'm a 17 year old student that is heading to the Univeristy of Waterloo in the fall, for the Honours Computer Science and Business Administration program! I have a keen interest in Computer Science, Mathematics and Business and hope to work for Google, Microsoft, Amazon, Apple or Facebook someday! After my undergraduate degree I also aspire to get my Masters in Business! I created this website after taking a course on <a href="http://www.codecademy.com/learn"> CodeAcademy </a> which really helped me out alot, so if you are new to programming I really encourage you to check it out! </p>
+    </div>
+    </div>
+
+    <div class="learn-more">
+	  <div class="container">
+		<div>
+	      <div>
+			<h3>Goals </h3>
+			<p> While learning Computer Science independantly, i wish to develop critical think and excellent problem solving skills. Before heading to university in the fall, I would like to learn basic web development as well as familiarize myself with Java and Scheme programming. There days I am also reading a book about Scheme program called "How to Design Programs" which I have also linked below.</p>
+			<p><a href="http://htdp.org/">How to Develop Programs</a></p>
+	      </div>
+		  <div>
+			<h3>Background</h3>
+			<p>I am originally from Pakistan and moved to Canada in 2011 and attended John Fraser Secondary School. Though I originally planned on studying Electrical Engineering, I later decided to go into CS/Business. I will be moving to Kitchener, Waterloo in the fall and staying on campus for my first year</p>
+			<p><a href="https://www.facebook.com/">Connect with me on Facebook!</a></p>
+		  </div>
+		  <div>
+			<h3>Side Projects and Goals</h3>
+			<p>I am very new to the world of Computer Science and programming, so I do not have an impressive list of side projects and apps. However I am more than willing to learn and have started looking into app development and various programming languages. Linked above is a link to my new GitHub account which I picked up recently. Feel free to follow me and watch my progress!</p>
+			<p><a href="https://github.com/shehryarmansoor">Shehryar Mansoor's GitHub Account </a></p>
+		  </div>
+	    </div>
+	  </div>
+	</div>
+  </body>
+</html>
