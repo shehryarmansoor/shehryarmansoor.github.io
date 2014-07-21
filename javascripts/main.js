99ab398bb353850ba0273e1d1dee27af0b729287
Shehryar Mansoor <shehryar.ahmad96@gmail.com>
2014-07-21 17:56:09 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,64 +1,71 @@
-<!DOCTYPE html>
-<html>
+.nav a {
+  color: #5a5a5a;
+  font-size: 12px;
+  font-weight: bold;
+  padding: 14px 10px;
+  text-transform: uppercase;
+}
 
-  <head>
-    <link href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/shift.css" rel="stylesheet">
-    
-    <link rel="stylesheet" href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/bootstrap.css">
-    <link rel="stylesheet" href="main.css">
-    
-  </head>
+.nav li {
+  display: inline;
+}
+  .nav { 
+      background-image:url('http://upload.wikimedia.org/wikipedia/commons/f/f1/Shades_of_light_blue.png')
+      
+}
 
-  <body>
-    <div class="nav">
-      <div class="container">
-     
-        <ul class="pull-right">
-        <ul>
-          <li><a href="#">About Me</a></li>
-          <li><a href="https://github.com/shehryarmansoor">GitHub </a></li>
-          <li><a href="https://uwaterloo.ca">University</a></li>
-          <li><a href="https://cs.uwaterloo.ca/"> Computer Science </a></li>
-        </ul>
-        </ul>
-      </div>
-    </div>
+.jumbotron {
+  background-image:url('http://www.engelbrechts.dk/sites/default/files/imagecache/800px/ocean%20blue%20color_1.jpg');
+  height: 500px;
+}
 
-    <div class="jumbotron">
-      <div class="container">
-        <h1>Shehryar Mansoor.</h1>
-        <p>A new and dedicated Computer Science student!</p>
-        <a href="https://www.facebook.com/Sherry.96">Get to know me!</a>
-      </div>
-    </div> 
-    
-    <div class="neighborhood-guide">
-    <div class="container">
-    <h2> Who am I? <h2>
-    <p> I'm a 17 year old student that is heading to the Univeristy of Waterloo in the fall, for the Honours Computer Science and Business Administration program! I have a keen interest in Computer Science, Mathematics and Business and hope to work for Google, Microsoft, Amazon, Apple or Facebook someday! After my undergraduate degree I also aspire to get my Masters in Business! I created this website after taking a course on <a href="http://www.codecademy.com/learn"> CodeAcademy </a> which really helped me out alot, so if you are new to programming I really encourage you to check it out! </p>
-    </div>
-    </div>
+.jumbotron .container {
+  position: relative;
+  top:100px;
+}
 
-    <div class="learn-more">
-	  <div class="container">
-		<div>
-	      <div>
-			<h3>Goals </h3>
-			<p> While learning Computer Science independantly, i wish to develop critical think and excellent problem solving skills. Before heading to university in the fall, I would like to learn basic web development as well as familiarize myself with Java and Scheme programming. There days I am also reading a book about Scheme program called "How to Design Programs" which I have also linked below.</p>
-			<p><a href="http://htdp.org/">How to Develop Programs</a></p>
-	      </div>
-		  <div>
-			<h3>Background</h3>
-			<p>I am originally from Pakistan and moved to Canada in 2011 and attended John Fraser Secondary School. Though I originally planned on studying Electrical Engineering, I later decided to go into CS/Business. I will be moving to Kitchener, Waterloo in the fall and staying on campus for my first year</p>
-			<p><a href="https://www.facebook.com/">Connect with me on Facebook!</a></p>
-		  </div>
-		  <div>
-			<h3>Side Projects and Goals</h3>
-			<p>I am very new to the world of Computer Science and programming, so I do not have an impressive list of side projects and apps. However I am more than willing to learn and have started looking into app development and various programming languages. Linked above is a link to my new GitHub account which I picked up recently. Feel free to follow me and watch my progress!</p>
-			<p><a href="https://github.com/shehryarmansoor">Shehryar Mansoor's GitHub Account </a></p>
-		  </div>
-	    </div>
-	  </div>
-	</div>
-  </body>
-</html>
+.jumbotron h1 {
+  color: #fff;
+  font-size: 89px;  
+  font-family: 'Shift', sans-serif;
+  font-weight: bold;
+}
+
+.jumbotron p {
+  font-size: 25px;
+  color: #fff;
+}
+
+.learn-more {
+  background-color: #f7f7f7;
+}
+
+.learn-more h3 {
+  font-family: 'Shift', sans-serif;
+  font-size: 18px;
+  font-weight: bold;
+}
+
+.learn-more a {
+  color: #00b0ff;
+}
+
+
+.neighborhood-guide {
+  background-image:url('http://hdwallsource.com/img/2014/5/light-blue-wallpaper-7851-8144-hd-wallpapers.jpg');
+  height: 500px;
+}
+
+.learn-more{
+    background-image:url('http://upload.wikimedia.org/wikipedia/commons/f/f1/Shades_of_light_blue.png')
+}
+
+.neighborhood-guide p {
+
+font-size: 34px;
+font-family:  'Shift', sans-serif;
+}
+
+
+
+
